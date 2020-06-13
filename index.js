661cf072770f2c78c09c6ab993822fa0c580d490
gribanovartem <gribanovartem22@gmail.com>
2020-06-13 14:01:55 +0300

    2

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 8003;
+
 app.get('/todos', (req, res)=> {
    console.log('asgfasdg');
    res.send('gggg')
 })
 
-app.listen('https://todoblognodejs.herokuapp.com');
+app.listen(PORT);
