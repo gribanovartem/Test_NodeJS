da8452f7cebbc95725b26b947b08723431400323
gribanovartem <gribanovartem22@gmail.com>
2020-06-13 18:26:05 +0300

    8

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
+let databaseTodos;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
 const PORT = process.env.PORT || 8003;
 
-const todos = [
-   { title: "Завершить список дел", id: 23523563463, completed: false },
-   { title: "Добавить раздел 'Информация'", id: 45673568356, completed: false },
-   { title: "Начать разработку блога", id: 35684578964, completed: false },
-];
+// const todos = [
+//    { title: "Завершить список дел", id: 23523563463, completed: false },
+//    { title: "Добавить раздел 'Информация'", id: 45673568356, completed: false },
+//    { title: "Начать разработку блога", id: 35684578964, completed: false },
+// ];
 
 app.get("/todos", (req, res) => {
-   return res.send(todos);
+   databaseTodos.find();
 });
 app.post("/todos", (req, res) => {
    todos = [...todos, req.body]
    res.send(req.body)
 });
 
-app.listen(PORT);
+const uri = "mongodb+srv://gribanovartem22:159159@reacttypescript-77iet.mongodb.net/ReactTypeScript?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  databaseTodos = client.db("ReactTypeScript").collection("Todos");
+  // perform actions on the collection object
+  app.listen(PORT);
+  client.close();
+});
