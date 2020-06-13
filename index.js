3b9091e1c6c556a51568c5ebb44cd7e897565697
gribanovartem <gribanovartem22@gmail.com>
2020-06-13 20:34:29 +0300

    8

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ let databaseTodos;
 let db;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   next();
+ });
 
 const PORT = process.env.PORT || 8003;
 
