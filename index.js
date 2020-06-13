23df76103f975925049516d2d155dbc88b440997
gribanovartem <gribanovartem22@gmail.com>
2020-06-13 17:19:45 +0300

    8

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ app.use(bodyParser.urlencoded({extended: true}))
 const PORT = process.env.PORT || 8003;
 
 const todos = [
-   { title: "gggggggggg", id: 23523563463, completed: false },
-   { title: "hhhhhhhhhh", id: 45673568356, completed: false },
-   { title: "aaaaaaaaaa", id: 35684578964, completed: false },
+   { title: "Завершить список дел", id: 23523563463, completed: false },
+   { title: "Добавить раздел 'Информация'", id: 45673568356, completed: false },
+   { title: "Начать разработку блога", id: 35684578964, completed: false },
 ];
 
 app.get("/todos", (req, res) => {
    return res.send(todos);
 });
 app.post("/todos", (req, res) => {
-   console.log(req.body);
+   todos = [...todos, req.body]
    res.send(req.body)
 });
 
