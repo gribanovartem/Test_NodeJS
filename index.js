949f67d60209d0a116d2cbb87bcc9d3ab6c41905
gribanovartem <gribanovartem22@gmail.com>
2020-06-13 23:07:09 +0300

    8

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ let db;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+let allowCrossDomain = function(req, res, next) {
+   res.header('Access-Control-Allow-Origin', "*");
+   res.header('Access-Control-Allow-Headers', "*");
+   next();
+ }
+ app.use(allowCrossDomain);
 
 const PORT = process.env.PORT || 8003;
 
