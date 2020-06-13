81b680c4310ee2365287568b3e3996b2bcce5f6f
gribanovartem <gribanovartem22@gmail.com>
2020-06-13 14:15:15 +0300

    3

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const PORT = process.env.PORT || 8003;
 
-app.get('/todos', (req, res)=> {
-   console.log('asgfasdg');
-   res.send('gggg')
-})
+const todos = [
+   { title: "gggggggggg", id: 23523563463, completed: false },
+   { title: "hhhhhhhhhh", id: 45673568356, completed: false },
+   { title: "aaaaaaaaaa", id: 35684578964, completed: false },
+];
+
+app.get("/todos", (req, res) => {
+   console.log("asgfasdg");
+   res.send(JSON.stringify(todos));
+});
 
 app.listen(PORT);
