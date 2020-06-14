4ec41d72ecb30281df42c0924b922eef06afefe8
gribanovartem <gribanovartem22@gmail.com>
2020-06-14 21:46:52 +0300

    9

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get("/todos", (req, res) => {
       collection.find({}).toArray(function (err, docs) {
          console.log("Found the following records");
          console.log(docs);
-         return res.json({aaa: 123});
+         return res.json(docs);
       });
       client.close();
    });
